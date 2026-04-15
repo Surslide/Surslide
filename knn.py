c0a9532cf4c2a8c0ab7793c7167e7b3445a5d24f
Luiz Gustavo <luizguspfc@gmail.com>
2026-04-15 08:23:04 -0300

    Make KNN implementation more reusable and easier to understand
    # Improvements
    - Improved variable naming
    - Simplified distance calculation into a single function
    - Made the implementation reusable regardless of the feature and label variable names

--- knn.py
+++ knn.py
@@ -1,36 +1,32 @@
 #KNearestNeighbors
 #Using Euclidean distance
 
-x = [[2,4],[7,8],[8,9],[1,3],[4,3],[6,5],[2,2],[9,5],[7,6],[1,4]]
-y = [0,1,1,0,0,1,0,1,1,0]
+x = [[1,2],[2,3],[2,4],[3,3],[3,4],[4,5],[5,5],[6,6],[6,7],[7,8],[8,8],[8,9]]
 
-#distance calculation
-def calculation(sample, point, index):
-    calc = []
-    for o in range(len(sample)):
-        calc.append((sample[o] - point[o]) **2)
-    return sum(calc) ** 0.5, y[index]
+y = [0,0,0,0,0,0,1,1,1,1,1,1]
 
-def distance(sample, features):
-    dist = []
-    for i, point in enumerate(features):
-        dist.append(calculation(sample, point, i))
-    return dist
-
-#prediction
-def predict(sample, k):
-    distance_classification = distance(sample, x)
-    distance_classification.sort()
-    kclassification = []
+def distance(features, labels, sample):
+    distance_value = []
+    for idx, point in enumerate(features):
+        sum_squared_distance = 0
+        for idx2, value in enumerate(point):
+            sum_squared_distance += (sample[idx2] - value) ** 2
+        distance_value.append([sum_squared_distance ** 0.5, labels[idx]])
+    return distance_value
+       
+def predict(sample, k, features, labels):
+    k_chosen = []
     count = 0
-    for j in range(k):
-        kclassification.append(distance_classification[j][1])
-    for val in kclassification:
-        if count < kclassification.count(val):
-            count = kclassification.count(val)
-            result = val
-    return result
+    distance_classification = distance(features, labels, sample)
+    distance_classification.sort()
+    for i in range(k):
+        k_chosen.append(distance_classification[i][1])
+    for o in k_chosen:
+        if k_chosen.count(o) > count:
+            count = k_chosen.count(o)
+            y_pred = o
+    return y_pred
 
-sample_y= predict([5,4], 3)
+sample_y = predict([5,6], 3, x, y)
 
 print("The most likely value of y for this sample is:", sample_y)
