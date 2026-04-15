684dea5505370859106c5828c780724d5a6fd5c1
Luiz Gustavo <luizguspfc@gmail.com>
2026-04-15 08:36:55 -0300

    Improve linear regression using grid search readability
    # Improvements
    - Improved variable and function naming for better readability
    - Replaced total squared error with mean squared error
    - Separated model creation from prediction logic
    - Made the implementation reusable with generic feature and label inputs
    - Added prediction support for unseen samples

--- Linear regression using grid search.py
+++ Linear regression using grid search.py
@@ -1,35 +1,36 @@
 #Linear Regression using grid search
 #Testing all possibilities to find the closest a, b
 
-#y = grades , x = hours of study
-x = [1,2,3,4,5]
-y = [19,36,53,70,87]
+x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+y = [15, 22, 29, 36, 43, 50, 57, 64, 71, 78]
 
-possible_a = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
-possible_b = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+#tries
+possible_a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+possible_b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
 
-def calculation(x, a, b):
-    y_calc = a * x + b
-    return y_calc
+def error_calculation(features, labels, a, b):
+    mean_squared_error = 0
+    for idx in range(len(labels)):
+        mean_squared_error += ((labels[idx] - (a * features[idx] + b)) ** 2) / len(labels)
+    return mean_squared_error
 
-def test_error(a, b):
-    total_error = 0
-    for i in range(len(y)):
-        total_error += (y[i] - calculation(x[i], a, b)) ** 2
-    return total_error
-
-#for each a, for each b, calculates the error and saves the best combination
-def best_parameters(test_a, test_b):
+def model_creation(features, labels, a_list, b_list):
     best_error = float("inf")
-    for a in test_a:
-        for b in test_b:
-            e = test_error(a, b)
-            if e < best_error:
-                best_error = e
-                best_test_a = a
-                best_test_b = b
-    return best_error, best_test_a, best_test_b
+    best_a = 0
+    best_b = 0
+    for a_attempt in a_list:
+        for b_attempt in b_list:
+            error = error_calculation(features, labels, a_attempt, b_attempt)
+            if error < best_error:
+                best_error = error
+                best_a = a_attempt
+                best_b = b_attempt
+    return best_a, best_b
 
-error, a, b = best_parameters(possible_a, possible_b)
+def predict(sample, a_b):
+    y_pred = a_b[0] * sample + a_b[1]
+    print(y_pred)
 
-print(f"The closest function is: y = {a}x + {b}\nTotal squared error of: {error}")
+lin_grid = model_creation(x, y, possible_a, possible_b)
+
+predict(11, lin_grid)
