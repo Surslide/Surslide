809487e354b682e7cada460ab8f15b9c4fe0be88
Luiz Gustavo <luizguspfc@gmail.com>
2026-04-17 19:53:33 -0300

    Update Gradient Descent
    - Added prediction and loss output
    - Switched training method to gradient-based updates
    - Made training configurable (learning rate and iterations)

--- Gradient Descent.py
+++ Gradient Descent.py
@@ -3,38 +3,37 @@
 x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 y = [11, 18, 25, 32, 39, 46, 53, 60, 67, 74]
 
-a_try = 3
-b_try = 9
-
-def loss(a, b, x, y):
+def loss(features, labels, a_b):
     mean_squared_error = 0
-    for idx in range(len(x)):
-        mean_squared_error += ((y[idx] - (a * x[idx] + b)) ** 2) / len(x)
-    return mean_squared_error
+    for idx in range(len(features)):
+        mean_squared_error += ((labels[idx] - (a_b[0] * features[idx] + a_b[1])) ** 2)
+    return mean_squared_error / len(features)
 
-def train_model(a_current, b_current, features, labels):
-    for _ in range(10000):
+def calculate_vote_a(a, b, features, labels):
+    vote_a = 0
+    for idx in range(len(features)):
+        vote_a += (labels[idx] - (a * features[idx] + b)) * features[idx]
+    return vote_a / len(features)
+
+def calculate_vote_b(a, b, features, labels):
+    vote_b = 0
+    for idx in range(len(features)):
+        vote_b += labels[idx] - (a * features[idx] + b)
+    return vote_b / len(features)
+
+def train_model(features, labels, learning_rate = 0.01, iterations=10000):
+    a_current = 1
+    b_current = 1
+    for _ in range(iterations):
         #training "a"
-        a_current_loss = loss(a_current, b_current, features, labels)
-        a_next_loss = loss(a_current + 0.01, b_current, features, labels)
-        a_improvement = a_current_loss - a_next_loss
-        if a_improvement > 0:
-            a_current += 0.01
-        elif a_improvement < 0:
-            a_current -= 0.01
+        a_current += calculate_vote_a(a_current, b_current, features, labels) * learning_rate
         #training "b"
-        b_current_loss = loss(a_current, b_current, features, labels)
-        b_next_loss = loss(a_current, b_current + 0.01, features, labels)
-        b_improvement = b_current_loss - b_next_loss
-        if b_improvement > 0:
-            b_current += 0.01
-        elif b_improvement < 0:
-            b_current -= 0.01       
+        b_current += calculate_vote_b(a_current, b_current, features, labels) * learning_rate
     return a_current, b_current
 
 def predict(sample, a_b):
     return a_b[0] * sample + a_b[1]
 
-model_params = train_model(a_try, b_try, x, y)
+model_params = train_model(x, y)
 
-print(predict(11, model_params))
+print(f"predict: {predict(11, model_params)}\nloss: {loss(x, y, model_params):.20f}")
