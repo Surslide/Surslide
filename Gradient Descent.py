b9a9b71d182ef4cbd687191ae10aefbc90a36bfb
Luiz Gustavo <luizguspfc@gmail.com>
2026-04-20 13:27:38 -0300

    Improved gradient descent structure
    - Combined the gradient into a single function
    - Updated parameters simultaneously
    - Improved code clarity and efficiency

--- Gradient Descent.py
+++ Gradient Descent.py
@@ -1,4 +1,4 @@
-#Gradient Descent
+#Gradient descent
 
 x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 y = [11, 18, 25, 32, 39, 46, 53, 60, 67, 74]
@@ -9,26 +9,21 @@ def loss(features, labels, a_b):
         mean_squared_error += ((labels[idx] - (a_b[0] * features[idx] + a_b[1])) ** 2)
     return mean_squared_error / len(features)
 
-def calculate_vote_a(a, b, features, labels):
-    vote_a = 0
+def gradient(a, b, features, labels):
+    a_grad = 0
+    b_grad = 0
     for idx in range(len(features)):
-        vote_a += (labels[idx] - (a * features[idx] + b)) * features[idx]
-    return vote_a / len(features)
+        error = labels[idx] - (a * features[idx] + b)
+        a_grad += error * features[idx]
+        b_grad += error
+    return a_grad / len(features), b_grad / len(features)
 
-def calculate_vote_b(a, b, features, labels):
-    vote_b = 0
-    for idx in range(len(features)):
-        vote_b += labels[idx] - (a * features[idx] + b)
-    return vote_b / len(features)
-
-def train_model(features, labels, learning_rate = 0.01, iterations=10000):
+def train_model(features, labels, learning_rate=0.01, iterations=10000):
     a_current = 1
     b_current = 1
     for _ in range(iterations):
-        #training "a"
-        a_current += calculate_vote_a(a_current, b_current, features, labels) * learning_rate
-        #training "b"
-        b_current += calculate_vote_b(a_current, b_current, features, labels) * learning_rate
+        a_grad, b_grad = gradient(a_current, b_current, features, labels)
+        a_current, b_current = a_current + a_grad * learning_rate, b_current + b_grad * learning_rate
     return a_current, b_current
 
 def predict(sample, a_b):
