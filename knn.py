05017f8b5bd4118ace28cc81286b565338efa789
Luiz Gustavo <luizguspfc@gmail.com>
2026-05-01 08:55:12 -0300

    Refactor KNN implementation (pure Python)
    - Improved code organization
    - Improved logic for selecting the k nearest neighbors
    - Made the code more readable and easier to understand

--- knn.py
+++ knn.py
@@ -5,28 +5,27 @@ x = [[1,2],[2,3],[2,4],[3,3],[3,4],[4,5],[5,5],[6,6],[6,7],[7,8],[8,8],[8,9]]
 
 y = [0,0,0,0,0,0,1,1,1,1,1,1]
 
-def distance(features, labels, sample):
-    distance_value = []
-    for idx, point in enumerate(features):
+def compute_distances(features, sample):
+    distances = []
+    for point in features:
         sum_squared_distance = 0
-        for idx2, value in enumerate(point):
-            sum_squared_distance += (sample[idx2] - value) ** 2
-        distance_value.append([sum_squared_distance ** 0.5, labels[idx]])
-    return distance_value
-       
-def predict(sample, k, features, labels):
-    k_chosen = []
+        for i in range(len(point)):
+            sum_squared_distance += (sample[i] - point[i]) ** 2
+        distances.append(sum_squared_distance ** 0.5)
+    return distances
+
+def get_k_nearest(distances, labels, k):
     count = 0
-    distance_classification = distance(features, labels, sample)
-    distance_classification.sort()
-    for i in range(k):
-        k_chosen.append(distance_classification[i][1])
-    for o in k_chosen:
-        if k_chosen.count(o) > count:
-            count = k_chosen.count(o)
-            y_pred = o
-    return y_pred
+    sorted_indices = sorted(range(len(distances)), key=lambda i: distances[i])
+    k_labels = [labels[i] for i in sorted_indices[:k]]
+    for lab in k_labels:
+        if k_labels.count(lab) > count:
+            count = k_labels.count(lab)
+            most_common = lab
+    return most_common
 
-sample_y = predict([5,6], 3, x, y)
+def predict(sample, features, labels, k=3):
+    distances = compute_distances(features, sample)
+    return get_k_nearest(distances, labels, k)
 
-print("The most likely value of y for this sample is:", sample_y)
+print("The most likely value of y for this sample is:", predict([5,6], x, y))
