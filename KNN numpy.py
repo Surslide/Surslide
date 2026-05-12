2bcde24c12aa028c62ac56d8b0c32427731f6a66
Luiz Gustavo <luizguspfc@gmail.com>
2026-05-12 11:02:05 -0300

    Refactor KNN implementation with classes
    - Updated KNN to use classes
    - Added weighted voting
    - Added Manhattan distance support
    - Improved readability and validation
    - Added __main__ guard

--- KNN numpy.py
+++ KNN numpy.py
@@ -1,26 +1,53 @@
 #KNearestNeighbors with external libraries
-#Using Euclidean distance
+#Euclidean or Manhattan distance
 
-from collections import Counter
 import numpy as np
 
-X = np.array([[1,2],[2,3],[2,4],[3,3],[3,4],[4,5],[5,5],[6,6],[6,7],[7,8],[8,8],[8,9]])
+X = [[1,2],[2,3],[2,4],[3,3],[3,4],[4,5],[5,5],[6,6],[6,7],[7,8],[8,8],[8,9]]
 
-y = np.array([0,0,0,0,0,0,1,1,1,1,1,1])
+y = [0,0,0,0,0,0,1,1,1,1,1,1]
 
-def compute_distances(features, sample):
-    distances = (features - sample) ** 2
-    distances = distances.sum(axis=1)
-    return np.sqrt(distances)
+def compute_distances(sample, features, distance_type):
+    if distance_type == "euclidean":
+        return np.sqrt(((sample - features) ** 2).sum(axis=1))
+    elif distance_type == "manhattan":
+        return np.abs(sample - features).sum(axis=1)
+    else:
+        raise ValueError("Invalid distance type")
 
-def get_k_nearest(distances, labels, k):
-    sorted_indices = np.argsort(distances)
-    k_labels = labels[sorted_indices[:k]]
-    return Counter(k_labels).most_common(1)[0][0]
+def predict_label(distances, labels, k):
+    epsilon = 0.00001
+    votes = {}
+    sorted_idx = np.argsort(distances)
+    k_idx = sorted_idx[:k]
+    for idx in k_idx:
+        if labels[idx] in votes:
+            votes[labels[idx]] += 1 / (distances[idx] + epsilon)
+        else:
+            votes[labels[idx]] = 1 / (distances[idx] + epsilon)
+    return max(votes, key=lambda i: votes[i])
 
-def predict(sample, features, labels, k=3):
-    sample = np.array(sample)
-    distances = compute_distances(features, sample)
-    return get_k_nearest(distances, labels, k)
+class KNN:
+    def __init__(self, k=3):
+        self.k = k
+        self.features = None
+        self.labels = None
 
-print("The most likely value of y for this sample is:", predict([5,6], X, y))
+    def fit(self, features, labels):
+        self.features = np.array(features)
+        self.labels = np.array(labels)
+        if self.k > len(self.labels):
+            raise ValueError("K cannot be higher than number of samples")
+    
+    def predict(self, samples, distance_type="euclidean"):
+        results = []
+        samples = np.array(samples)
+        if self.features is None or self.labels is None:
+            raise ValueError("Model hasn't been fitted yet")
+        results = [predict_label(compute_distances(sample, self.features, distance_type), self.labels, self.k) for sample in samples]
+        return np.array(results)
+
+if __name__ == "__main__":
+    KNN_3 = KNN(3)
+    KNN_3.fit(X, y)
+    print(KNN_3.predict([[5,5],[8,4]], "euclidean"))
