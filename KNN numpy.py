da2f3824778db186eaf0633fadd965926383c49b
Luiz Gustavo <luizguspfc@gmail.com>
2026-07-30 19:08:07 -0300

    Update KNN numpy.py
    Small fixes

--- KNN numpy.py
+++ KNN numpy.py
@@ -3,17 +3,15 @@
 
 import numpy as np
 
-X = [[1,2],[2,3],[2,4],[3,3],[3,4],[4,5],[5,5],[6,6],[6,7],[7,8],[8,8],[8,9]]
-
-y = [0,0,0,0,0,0,1,1,1,1,1,1]
-
 def compute_distances(sample, features, distance_type):
+    distance_type = distance_type.lower()
+    distance_type = distance_type.strip()
     if distance_type == "euclidean":
         return np.sqrt(((sample - features) ** 2).sum(axis=1))
     elif distance_type == "manhattan":
         return np.abs(sample - features).sum(axis=1)
     else:
-        raise ValueError("Invalid distance type")
+        raise ValueError("Invalid distance type, this model only supports 'euclidean' or 'manhattan'")
 
 def predict_label(distances, labels, k):
     epsilon = 0.00001
@@ -29,6 +27,8 @@ def predict_label(distances, labels, k):
 
 class KNN:
     def __init__(self, k=3):
+        if not (isinstance(k, int) and k > 0):
+            raise ValueError("K must be an 'int' and must be greater than '0'")
         self.k = k
         self.features = None
         self.labels = None
@@ -36,18 +36,32 @@ class KNN:
     def fit(self, features, labels):
         self.features = np.array(features)
         self.labels = np.array(labels)
+        if len(self.features) != len(self.labels):
+            raise ValueError("Features and labels must have the same size")
         if self.k > len(self.labels):
             raise ValueError("K cannot be higher than number of samples")
     
     def predict(self, samples, distance_type="euclidean"):
-        results = []
         samples = np.array(samples)
+        if samples.ndim == 1:
+            samples = samples.reshape(1, -1)
+        elif samples.ndim > 2: 
+            raise ValueError("Samples must have shape (samples, features)")
         if self.features is None or self.labels is None:
             raise ValueError("Model hasn't been fitted yet")
+        
         results = [predict_label(compute_distances(sample, self.features, distance_type), self.labels, self.k) for sample in samples]
         return np.array(results)
 
 if __name__ == "__main__":
+    #features
+    X = [[1,2],[2,3],[2,4],[3,3],[3,4],[4,5],[5,5],[6,6],[6,7],[7,8],[8,8],[8,9]]
+
+    #labels
+    y = [0,0,0,0,0,0,1,1,1,1,1,1]
+
     KNN_3 = KNN(3)
     KNN_3.fit(X, y)
-    print(KNN_3.predict([[5,5],[8,4]], "euclidean"))
+    print(KNN_3.predict([5,5], "euclidean"))
+    print()
+    print(KNN_3.predict([[3,5],[1,2],[8,6]], "manhattan"))
